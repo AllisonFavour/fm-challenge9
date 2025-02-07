2391f3484d22a4c1fa40b2a5dbcbb6adb1cd01fa
Allison Favour <allisonfavour@outlook.com>
2025-02-07 13:27:09 -0800

    completed the desktop and mobile layout, about to work on the interactivity

---
+++ main.js
@@ -0,0 +1,20 @@
+// const mainElement = document.querySelector('main');
+// const form = document.getElementById('js-form');
+// const popUpMenu = document.getElementById('js-pop-up');
+// console.log(mainElement);
+
+// form.addEventListener('submit', handleForm);
+
+// function handleForm(e) {
+//     e.preventDefault();
+
+//     mainElement.style.display = 'none';
+
+
+//     // popUpMenu.classList.toggle('show');
+//     popUpMenu.style.display = 'block';
+
+// }
+
+// // dynamically render and update footer year
+// document.getElementById('js-footer-year').innerText = new Date().getFullYear();
